1764ebfbdb713f70e1aa6c2b97041bd7f9371295
Vineeth Vijayan <vneethv@gmail.com>
2025-10-08 07:19:19 +0200

    apex_tr1: Added plot
    Signed-off-by: Vineeth Vijayan <vneethv@gmail.com>

--- apex_tr1/airline_dataset_downloader.py
+++ apex_tr1/airline_dataset_downloader.py
@@ -1,8 +1,11 @@
 import os
+import pandas as pd
+import matplotlib.pyplot as graph
 from kaggle.api.kaggle_api_extended import KaggleApi
 
+
 os.environ['KAGGLE_USERNAME'] = 'vneethv'
-os.environ['KAGGLE_KEY'] = 'xxx' #Removed
+os.environ['KAGGLE_KEY'] = 'xxx' #replace
 
 api = KaggleApi()
 api.authenticate()
@@ -12,7 +15,30 @@ path = './datasets'
 
 os.makedirs(path, exist_ok=True)
 
+# download from kaggle
+
 api.dataset_download_files(airline_dataset, path=path, unzip=True)
 
 print(f"Airline dataset at: {path}")
+
+datafile = 'airline-passengers.csv'
+file_path = os.path.join(path, datafile)
+df = pd.read_csv(file_path)
+
+#shoe couple of lines
+
+print(df.head())
+
+#plot it
+
+df['month'] = pd.to_datetime(df['month'])
+
+graph.figure(figsize=(12,6))
+graph.plot(df['month'], df['total_passengers'], marker='o', color='blue')
+graph.title("Air Passengers (1949-1960)")
+graph.xlabel("month")
+graph.ylabel("No of Passengers")
+graph.xticks(rotation=100)
+graph.grid(True)
+graph.show()
 
